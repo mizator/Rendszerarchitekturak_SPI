0e1a3133a30dd6094c9de95fb7e3d70e07b0a6c2
tibortth4 <tibor.tth4@gmail.com>
2017-05-19 01:11:19 +0200

    statelogic added, spi_if ack_gen

--- src/spi_if.v
+++ src/spi_if.v
@@ -1,21 +1,21 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:     00:57:30 03/04/2017 
+// Company:
+// Engineer:
+//
+// Create Date:     00:57:30 03/04/2017
 // Design Name: 	Wishbone - Spi interface
 // Module Name:     spi_if
 // Project Name: 	Wishbone - Spi interface
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Target Devices:
+// Tool versions:
+// Description:
 //
-// Dependencies: 
+// Dependencies:
 //
-// Revision: 
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
 module spi_if(
@@ -33,17 +33,28 @@ module spi_if(
 	output 			ack, 		// Acknowledge
 	output 			irq,		// Interrupt request
 
-	// SPI signals 
-    input  			SPI_MISO,	// Master-In-Slave-Out	
-    output 			SPI_MOSI,	// Master-Out-Slave-In
-    output 			SPI_SCK,	// Bus-Clock
-    output 			SPI_nSS		// Slave-Select
+	// SPI signals
+  input  			SPI_MISO,	// Master-In-Slave-Out
+  output 			SPI_MOSI,	// Master-Out-Slave-In
+  output 			SPI_SCK,	// Bus-Clock
+  output 			SPI_nSS		// Slave-Select
 );
+
+//---------------------------------------------
+// States
+//---------------------------------------------
+parameter S_IDLE = 3'b000; //Idle
+parameter S_REG_RD = 3'b001; //Register read
+parameter S_REG_WR = 3'b010; //Register write
+parameter S_DOUT = 3'b011; //Data out
+parameter S_DIN = 3'b100; //Data out
+//---------------------------------------------
+
 //---------------------------------------------
 // Registers for SPI
 //---------------------------------------------
-reg [7:0] SPICR;		//SPI CONTROL REGISTER 		[Global EN | Interrupt EN | Write EN | Read EN]	
-reg [7:0] SPIBR;		//SPI BAUD-RATE Registers	[8bit] 
+reg [7:0] SPICR;		//SPI CONTROL REGISTER 		[Global EN | Interrupt EN | Write EN | Read EN]
+reg [7:0] SPIBR;		//SPI BAUD-RATE Registers	[8bit]
 reg [7:0] SPISR;		//SPI STATUS REGISTER 		[BUSY (reg/wire??) | Interrupt Clear (state)? | ]
 reg [7:0] SPIDR;		//SPI DATA REGISTER 		[]
 //---------------------------------------------
@@ -51,7 +62,7 @@ reg [7:0] SPIDR;		//SPI DATA REGISTER 		[]
 //---------------------------------------------
 // Register value settings
 //---------------------------------------------
-always @ (posedge clk) 
+always @ (posedge clk)
 begin
 	if(rst) begin
 		SPICR <= 8'b0;
@@ -63,6 +74,18 @@ begin
 	end
 end
 //---------------------------------------------
+
+//---------------------------------------------
+// next_state_en signal generation
+//---------------------------------------------
+reg next_state_en;
+always @ (posedge clk)
+begin
+		if (rst)
+			next_state_en <= 1'b1;
+		else if (spi_state != S_IDLE)
+			next_state_en <= 1'b0;	
+end
 
 //---------------------------------------------
 // SCK frequency divider module instantiation
@@ -80,23 +103,20 @@ sckgen spi_sckgen (
 //---------------------------------------------
 
 //---------------------------------------------
-// State logic + conditions???
+// State logic instantiation
 //---------------------------------------------
-
-
-//---------------------------------------------
-// Acknowledge signal generation
-//---------------------------------------------
-reg ack_reg;
-always @ (posedge clk)
-begin
-	if(rst)
-		ack_reg <= 1'b0;
-	else
-		ack_reg <= ;							//feltétel?
-end
-
-assign ack = ack_reg;
+reg [2:0] spi_state;
+statelogic statelogic (
+ 	.clk(clk), 		// System clock
+	.rst(rst), 		// System reset
+	.sck_rise(spi_sck_rise), // sck rising edge
+ 	.sck_fall(spi_sck_fall), // sck falling edge
+	.cmd(cmd), 		// Modify settings
+	.wr(wr), 		// Write data
+	.rd(rd), 		// Read data
+	.next_state_en(next_state_en), //inkább kellene egy transmit_finished flag vagy ilyesmi
+	.state(spi_state)
+);
 //---------------------------------------------
 
 //---------------------------------------------
@@ -113,6 +133,21 @@ shr spi_shr (
 	.dout(SPI_MOSI),
 	.dstr(spi_shr_dout)
 );
+//---------------------------------------------
+
+//---------------------------------------------
+// Acknowledge signal generation
+//---------------------------------------------
+reg ack_reg;
+always @ (*)
+begin
+	if(rst || spi_state == S_IDLE)
+		ack_reg <= 1'b0;
+	else if ((spi_state != S_IDLE) && next_state_en)
+		ack_reg <= 1'b0;
+end
+
+assign ack = ack_reg;
 //---------------------------------------------
 
 //---------------------------------------------
---
+++ src/statelogic.v
@@ -0,0 +1,80 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date:     13:52:01 04/16/2017
+// Design Name: 	Wishbone - Spi interface
+// Module Name:   	statelogic
+// Project Name: 	Wishbone - Spi interface
+// Target Devices:
+// Tool versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+module statelogic(
+	input 			clk, 		// System clock
+	input 			rst, 		// System reset
+	input 			sck_rise, // sck rising edge
+	input 			sck_fall, // sck falling edge
+	input 			cmd, 		// Modify settings
+	input 			wr, 		// Write data
+	input 			rd, 		// Read data
+	input 			next_state_en,
+
+	output 			[2:0] state 	// sck falling edge
+);
+
+reg [2:0] state_reg;
+assign state = state_reg;
+
+//---------------------------------------------
+// States
+//---------------------------------------------
+parameter S_IDLE = 3'b000; //Idle
+parameter S_REG_RD = 3'b001; //Register read
+parameter S_REG_WR = 3'b010; //Register write
+parameter S_DOUT = 3'b011; //Data out
+parameter S_DIN = 3'b100; //Data out
+//---------------------------------------------
+
+//---------------------------------------------
+// Next state logic
+//---------------------------------------------
+always @ (posedge clk)
+begin
+	if (rst)
+		state_reg <= S_IDLE;
+	else if (next_state_en) //next_state_en: jump to next state
+	begin
+		case (state_reg)
+			S_IDLE: begin
+				if (cmd & rd & !wr)
+					state_reg <= S_REG_RD;
+				else if (cmd & !rd & wr)
+					state_reg <= S_REG_WR;
+				else if (!cmd & rd & !wr)
+					state_reg <= S_DIN;
+				else if (!cmd & !rd & wr)
+					state_reg <= S_DOUT;
+			end
+
+			default: S_IDLE;
+		endcase
+end
+
+//---------------------------------------------
+
+//---------------------------------------------
+// Output signal generation
+//---------------------------------------------
+
+//---------------------------------------------
+
+endmodule
